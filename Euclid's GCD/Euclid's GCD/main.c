e07ddace6b041b25a13bd43dab013360bbbaa79d
jh0152park <jh0152park@naver.com>
2024-04-09 17:55:19 +0900

    added GCD logic

--- Euclid's GCD/Euclid's GCD/main.c
+++ Euclid's GCD/Euclid's GCD/main.c
@@ -7,8 +7,22 @@
 
 #include <stdio.h>
 
+int get_gcd(int u, int v) {
+    if(u < 0 || v < 0) {
+        return 0;
+    }
+    
+    int t;
+    while (v) {
+        t = u % v;
+        u = v;
+        v = t;
+    }
+    return u;
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    printf("280 and 30's GCD is %d\n", get_gcd(280, 30));
+    printf("170 and 15's GCD is %d\n", get_gcd(170, 15));
     return 0;
 }
