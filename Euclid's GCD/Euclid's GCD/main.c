66e87763bd2c139a1f4ddc44dfb5b06cfa3a36fe
jh0152park <jh0152park@naver.com>
2024-04-09 17:33:35 +0900

    created a new project

---
+++ Euclid's GCD/Euclid's GCD/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Euclid's GCD
+//
+//  Created by 박재현 on 2024/04/09.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
