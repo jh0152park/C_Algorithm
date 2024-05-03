8b2b0477c69c2bcc8352abfca8f9f67b8f2bbe28
jh0152park <jh0152park@naver.com>
2024-05-04 01:34:00 +0900

    added a new project

---
+++ Array/Array/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Array
+//
+//  Created by 박재현 on 2024/05/04.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
