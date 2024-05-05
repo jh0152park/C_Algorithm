d0fa566307de48fe9501b2451a097717a29baa74
jh0152park <jh0152park@naver.com>
2024-05-06 02:27:27 +0900

    added extra ocde

--- Double Linked List/Double Linked List/main.c
+++ Double Linked List/Double Linked List/main.c
@@ -79,8 +79,119 @@ int delete_node(int k) {
     return 0;
 }
 
+// t를 갖고있는 node 앞에 k를 갖는 노드를 삽입
+node *insert_node(int k, int t) {
+    node *s;
+    node *i = NULL;
+    
+    s = find_node(t);
+    if(s != tail) {
+        i = (node *)malloc(sizeof(node));
+        i->key = k;
+        
+        s->prev->next = i;
+        i->prev = s->prev;
+        s->prev = i;
+        i->next = s;
+    }
+    
+    return i;
+}
+
+node *ordered_insert(int k) {
+    node *s;
+    node *i;
+    
+    s = head->next;
+    while(s -> key <= k && s != tail) {
+        s = s->next;
+    }
+    
+    i = (node *)malloc(sizeof(node));
+    i->key = k;
+    s->prev->next = i;
+    i->prev = s->prev;
+    s->prev = i;
+    i->next = s;
+    
+    return i;
+}
+
+void print_list(node *p) {
+    printf("\n");
+    while(p != tail) {
+        printf("%-8d", p->key);
+        p = p->next;
+    }
+    printf("\n");
+}
+
+void delete_all(void) {
+    node *p;
+    node *s;
+    
+    p = head->next;
+    while(p != tail) {
+        s = p;
+        p = p->next;
+        free(s);
+    }
+    head->next = tail;
+    tail->prev = head;
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    node *t;
+    
+    init_list();
+    
+    ordered_insert(10);
+    ordered_insert(5);
+    ordered_insert(8);
+    ordered_insert(3);
+    ordered_insert(1);
+    ordered_insert(7);
+    ordered_insert(8);
+    
+    printf("\nInitial Linked list status as below");
+    print_list(head->next);
+    
+    printf("\nFinding 4 is %ssuccessful\n", find_node(4) == tail ? "un" : "");
+    
+    t = find_node(5);
+    printf("\nFinding 5 is %ssuccessful\n", t == tail ? "un" : "");
+    
+    printf("\nInserting 7 before 5");
+    insert_front(7, t);
+    print_list(head->next);
+    
+    t = find_node(3);
+    printf("\nDeleting 3");
+    delete_node_ptr(t);
+    print_list(head->next);
+    
+    printf("\nInserting node 2 before 10");
+    insert_node(2, 10);
+    print_list(head->next);
+    
+    printf("\nDeleting node 2");
+    if(!delete_node(2)) {
+        printf("\nDeleting 2 is fail");
+    }
+    print_list(head->next);
+    
+    printf("\nDeleting node 1");
+    delete_node(1);
+    print_list(head->next);
+    
+    printf("\nInserting 15 at first");
+    insert_front(15, head->next);
+    print_list(head->next);
+    
+    printf("\nDeleting all node");
+    delete_all();
+    print_list(head->next);
+    
+    
     return 0;
 }
