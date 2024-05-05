6761fa934801826fce5b885384f87c2182037830
jh0152park <jh0152park@naver.com>
2024-05-06 00:16:34 +0900

    added init, insert_front

--- Double Linked List/Double Linked List/main.c
+++ Double Linked List/Double Linked List/main.c
@@ -6,6 +6,44 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _node {
+    int key;
+    struct _node *prev;
+    struct _node *next;
+} node;
+
+node *head;
+node *tail;
+
+void init_list(void) {
+    head = (node *)malloc(sizeof(node));
+    tail = (node *)malloc(sizeof(node));
+    
+    head->prev = head;
+    head->next = tail;
+    tail->prev = head;
+    tail->next = tail;
+}
+
+node *insert_front(int key, node *t) {
+    node *new;
+    
+    if(t == head) {
+        return NULL;
+    }
+    
+    new = (node *)malloc(sizeof(node));
+    
+    new->key = key;
+    t->prev->next = new;
+    new->prev = t->prev;
+    t->prev = new;
+    new->next = t;
+    
+    return new;
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
