2091d31e930bdaee22d1f8e13f3cd29f3fff533e
jh0152park <jh0152park@naver.com>
2024-05-30 21:38:32 +0900

    created a new project

---
+++ Sort/Selection Sort/Selection Sort/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Selection Sort
+//
+//  Created by 박재현 on 2024/05/30.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
