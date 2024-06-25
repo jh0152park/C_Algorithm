cdd965a8a106bac7dd35e8da10a9b08b2f1c07f0
jh0152park <jh0152park@naver.com>
2024-06-25 23:18:00 +0900

    created a new project

---
+++ Sort/ShellSort/ShellSort/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ShellSort
+//
+//  Created by 박재현 on 2024/06/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
