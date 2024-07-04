e525b469f3c3538aa6a2c2e936d674d6898d4a67
jh0152park <jh0152park@naver.com>
2024-07-04 16:36:38 +0900

    added merge sort

---
+++ Sort/MergeSort/MergeSort/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  MergeSort
+//
+//  Created by 박재현 on 2024/07/04.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
